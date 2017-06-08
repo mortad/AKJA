5b60a5da8c8539fde0a672f37d9b85fb12e09f65
mortad <qaism474@gmail.com>
2017-06-08 06:24:03 +0300

    Create AKJA-Auto.sh

--- AKJA-Auto.sh
+++ AKJA-Auto.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./AKJA.sh
-curl "https://api.telegram.org/bot[362136053:AAGkJVHSOaAHFADlrQmKDn28AGLDNIbEA5c]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#AKJA-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot[362136053:AAGkJVHSOaAHFADlrQmKDn28AGLDNIbEA5c]/sendmessage" -F "chat_id=388859986" -F "text=#NEWCRASH-#AKJA-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
